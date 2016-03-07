3df82e769e4647bb92588632fb6333bb1819195c
Gustavo Wagner Diniz Mendes <gugawag@gmail.com>
2016-03-07 17:52:04 -0300

    pipe em c.

--- unix_pipe.c
+++ unix_pipe.c
@@ -19,44 +19,44 @@
 
 int main(void)
 {
-	char write_msg[BUFFER_SIZE] = "Greetings";
+	char write_msg[BUFFER_SIZE] = "Olá";
 	char read_msg[BUFFER_SIZE];
 	pid_t pid;
 	int fd[2];
 
-	/* create the pipe */
+	/* cria um pipe */
 	if (pipe(fd) == -1) {
-		fprintf(stderr,"Pipe failed");
+		fprintf(stderr,"Pipe falhou");
 		return 1;
 	}
 
-	/* now fork a child process */
+	/* cria um processo filho */
 	pid = fork();
 
 	if (pid < 0) {
-		fprintf(stderr, "Fork failed");
+		fprintf(stderr, "Fork falhou");
 		return 1;
 	}
 
-	if (pid > 0) {  /* parent process */
-		/* close the unused end of the pipe */
+	if (pid > 0) {  /* processo pai */
+		/* fecha o read_end do pipe, pois não será utilizado pelo pai */
 		close(fd[READ_END]);
 
-		/* write to the pipe */
+		/* escreve no pipe */
 		write(fd[WRITE_END], write_msg, strlen(write_msg)+1); 
 
-		/* close the write end of the pipe */
+		/* fecha o write_end do pipe */
 		close(fd[WRITE_END]);
 	}
-	else { /* child process */
-		/* close the unused end of the pipe */
+	else { /* processo filho */
+		/* fecha o write_end do pipe, pois não será usado pelo filho */
 		close(fd[WRITE_END]);
 
-		/* read from the pipe */
+		/* lê do pioe */
 		read(fd[READ_END], read_msg, BUFFER_SIZE);
 		printf("child read %s\n",read_msg);
 
-		/* close the write end of the pipe */
+		/* fecha o read_end do pipe */
 		close(fd[READ_END]);
 	}
 
