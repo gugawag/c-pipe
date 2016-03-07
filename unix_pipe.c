dd76a9e7c2d687991ed1fac0328e0ffb2fc11a7a
Gustavo Wagner Diniz Mendes <gugawag@gmail.com>
2016-03-07 17:58:32 -0300

    Colocadas mais mensagens.

--- unix_pipe.c
+++ unix_pipe.c
@@ -39,25 +39,30 @@ int main(void)
 	}
 
 	if (pid > 0) {  /* processo pai */
+		printf("=== Pai ===\n");
 		/* fecha o read_end do pipe, pois não será utilizado pelo pai */
 		close(fd[READ_END]);
 
+		printf("=== Escrevendo para meu filho: %s ===\n",write_msg);
 		/* escreve no pipe */
 		write(fd[WRITE_END], write_msg, strlen(write_msg)+1); 
 
 		/* fecha o write_end do pipe */
 		close(fd[WRITE_END]);
+		printf("=== Fim Pai ===\n");
 	}
 	else { /* processo filho */
+		printf("=== Filho ===\n");
 		/* fecha o write_end do pipe, pois não será usado pelo filho */
 		close(fd[WRITE_END]);
 
 		/* lê do pioe */
 		read(fd[READ_END], read_msg, BUFFER_SIZE);
-		printf("child read %s\n",read_msg);
+		printf("Eu li: %s\n",read_msg);
 
 		/* fecha o read_end do pipe */
 		close(fd[READ_END]);
+		printf("=== Fim Filho ===");
 	}
 
 	return 0;
